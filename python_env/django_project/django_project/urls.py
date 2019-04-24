ba608223ab1844422a5524d93db76b846f8f80a6
400209661 <antemiaa@mcmaster.ca>
2019-04-24 21:40:39 +0000

    Created Django Project03 Directory and activated

--- python_env/django_project/django_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.http import HttpResponse
-
-urlpatterns = [
-    path('e/antemiaa/' , include('elmapp.urls')) ,
-]
