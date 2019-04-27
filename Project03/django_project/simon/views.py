69ab3aad86a2427572702517df9430b5e42aeb1b
400209661 <antemiaa@mcmaster.ca>
2019-04-27 14:54:23 -0400

    Fixed messed up dictionaries

--- Project03/django_project/simon/views.py
+++ Project03/django_project/simon/views.py
@@ -11,14 +11,14 @@ scores = [
         'username': 'Jane',
         'score': '100',
         'date_posted': 'August 28 2018'
-    },
+    }
 ]
 
 def home(request):
     context = {
         'scores': scores
     }
-    return render(request, 'simon/home.html', scores)
+    return render(request, 'simon/home.html', context)
 
 def about(request):
     return render(request, 'simon/about.html')
