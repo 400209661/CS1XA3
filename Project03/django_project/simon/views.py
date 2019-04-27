34afc625d373e1555f4c85581189f94a3617a63a
400209661 <antemiaa@mcmaster.ca>
2019-04-27 14:42:50 -0400

    Added varriables acessable from db

--- Project03/django_project/simon/views.py
+++ Project03/django_project/simon/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 
+
+scores = [
+    {
+        'username': 'Alex',
+        'score': '101',
+        'date_posted': 'August 27 2018'
+    },
+    {
+        'username': 'Jane',
+        'score': '100',
+        'date_posted': 'August 28 2018'
+    },
+]
+
 def home(request):
-    return render(request, 'simon/home.html')
+    context = {
+        'scores': scores
+    }
+    return render(request, 'simon/home.html', scores)
 
 def about(request):
     return render(request, 'simon/about.html')
