00ec866de0188fb2da0705557cfbb171ec0de5a6
400209661 <antemiaa@mcmaster.ca>
2019-04-28 04:20:16 -0400

    Added List View to /home

--- Project03/django_project/simon/urls.py
+++ Project03/django_project/simon/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . views import ScoreListView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='simon-home'),
+    path('', ScoreListView.as_view(), name='simon-home'),
     path('about/', views.about, name='about-page'),
 ]
--- Project03/django_project/simon/views.py
+++ Project03/django_project/simon/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from .models import Score
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
 
 def home(request):
     leaderBoard = {
         'scores': Score.objects.all()
     }
     return render(request, 'simon/home.html', leaderBoard)
+
+class ScoreListView(ListView):
+    model = Score
+    template_name = 'simon/home.html'
+    context_object_name = 'scores'
 
 @login_required
 def about(request):
