3673aa86f356fd9830616d58446b88a7ae5c47d5
400209661 <antemiaa@mcmaster.ca>
2019-04-27 21:26:48 -0400

    Changed the int feild in score to a char to display it.

--- Project03/django_project/simon/models.py
+++ Project03/django_project/simon/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Score(models.Model):
-    score = models.IntegerField()
+    score = models.CharField()
     date_posted = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.score
-        
