449fa541bc86025c6fe421fd8f892b28c5c77bcb
400209661 <antemiaa@mcmaster.ca>
2019-04-28 06:13:11 -0400

    Edited the view.py function to contain the new view function pieFact and about.html to load urls

--- Project03/django_project/simon/views.py
+++ Project03/django_project/simon/views.py
@@ -3,6 +3,15 @@ from .models import Score
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
 
+def my_view_that_updates_pieFact(request):
+    if request.method == 'POST':
+        if 'score' in request.POST:
+            score = request.POST['score']
+            # doSomething with pieFact here...
+            return HttpResponse('success') # if everything is OK
+    # nothing went well
+    return HttpRepsonse('FAIL!!!!!')
+    
 def home(request):
     leaderBoard = {
         'scores': Score.objects.all()
@@ -18,12 +27,3 @@ class ScoreListView(ListView):
 @login_required
 def about(request):
     return render(request, 'simon/about.html', {'title': 'Game'})
-
-def my_view_that_updates_pieFact(request):
-    if request.method == 'POST':
-        if 'score' in request.POST:
-            score = request.POST['score']
-            # doSomething with pieFact here...
-            return HttpResponse('success') # if everything is OK
-    # nothing went well
-    return HttpRepsonse('FAIL!!!!!')
