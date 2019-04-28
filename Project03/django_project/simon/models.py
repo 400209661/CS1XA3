68a06ad2996f6ae8d504d3fb548b287894390660
400209661 <antemiaa@mcmaster.ca>
2019-04-27 21:04:01 -0400

    Edited the models.py file to contain score date_posted and author

--- Project03/django_project/simon/models.py
+++ Project03/django_project/simon/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.nodels import User
 # Create your models here.
+class Score(models.Model):
+    score = models.IntegerField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
