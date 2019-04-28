e828550deda50eb1d9b98c98a3580fdf134476c3
400209661 <antemiaa@mcmaster.ca>
2019-04-27 22:28:13 -0400

    Added confirmation messages for account creation.

--- Project03/django_project/users/views.py
+++ Project03/django_project/users/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UsersCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Created for {username}!')
+            return redirect('/e/antemiaa/')
+    else:
+        form = UserCreationForm()
+
     return render(request, 'users/register.html', {'form': form})
