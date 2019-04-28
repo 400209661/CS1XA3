ac15cb32f9257e95caf3d04abdda24a00e19a00d
400209661 <antemiaa@mcmaster.ca>
2019-04-28 05:32:32 -0400

    Fixed to Most recent

--- Project03/django_project/simon/views.py
+++ Project03/django_project/simon/views.py
@@ -13,7 +13,7 @@ class ScoreListView(ListView):
     model = Score
     template_name = 'simon/home.html'
     context_object_name = 'scores'
-    ordering = ['asc']
+    ordering = ['-date_posted']
 
 @login_required
 def about(request):
