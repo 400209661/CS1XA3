5e7f08aab7746d796d4e4dc142a013c991dc7255
400209661 <antemiaa@mcmaster.ca>
2019-04-28 06:51:00 -0400

    Adding Json requests

--- Project03/django_project/django_project/urls.py
+++ Project03/django_project/django_project/urls.py
@@ -24,5 +24,13 @@ urlpatterns = [
     path('e/antemiaa/login/', auth_views.LoginView.as_view(template_name= 'users/login.html'), name ='login'),
     path('e/antemiaa/logout/', auth_views.LogoutView.as_view(template_name= 'users/logout.html'), name ='logout'),
     path('e/antemiaa/profile/', user_views.profile, name ='profile'),
+    path('e/antemiaa/password-reset/',
+    auth_views.PasswordResetView.LogoutView.as_view(template_name= 'users/password_reset.html'),
+    name ='password_reset'),
+
+    path('e/antemiaa/password-reset/done',
+    auth_views.PasswordResetViewDone.LogoutView.as_view(template_name= 'users/password_reset_done.html'),
+    name ='password_reset_done'),
+
     path('e/antemiaa/', include('simon.urls'))
 ]
