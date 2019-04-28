615861a5c85bc9dd685eb7ea6d071b0ff1df66f8
400209661 <antemiaa@mcmaster.ca>
2019-04-27 21:52:05 -0400

    Edited Admin site to contain access to model - Score

--- Project03/django_project/simon/views.py
+++ Project03/django_project/simon/views.py
@@ -1,18 +1,6 @@
 from django.shortcuts import render
 from .models import Score
 
-scores = [
-    {
-        'username': 'Alex',
-        'score': '99',
-        'date_posted': 'April 27 2019'
-    },
-    {
-        'username': 'Jane',
-        'score': '100',
-        'date_posted': 'August 28 2019'
-    }
-]
 
 def home(request):
     leaderBoard = {
