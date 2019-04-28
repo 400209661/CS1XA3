8857a31bff2bca41ad8491d4e46a212eb15c140b
400209661 <antemiaa@mcmaster.ca>
2019-04-27 22:17:56 -0400

    Fixed Syntax Error for settings.py added page

--- Project03/django_project/django_project/settings.py
+++ Project03/django_project/django_project/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UserConfig',
+    'users.apps.UsersConfig',
     'simon.apps.SimonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
