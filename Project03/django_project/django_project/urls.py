d2cf2cf8626e875030652d4859cc405d9111584b
400209661 <antemiaa@mcmaster.ca>
2019-04-28 06:56:09 -0400

    Fixed urls.py - removed incorrect syntax

--- Project03/django_project/django_project/urls.py
+++ Project03/django_project/django_project/urls.py
@@ -25,11 +25,11 @@ urlpatterns = [
     path('e/antemiaa/logout/', auth_views.LogoutView.as_view(template_name= 'users/logout.html'), name ='logout'),
     path('e/antemiaa/profile/', user_views.profile, name ='profile'),
     path('e/antemiaa/password-reset/',
-    auth_views.PasswordResetView.LogoutView.as_view(template_name= 'users/password_reset.html'),
+    auth_views.PasswordResetView.as_view(template_name= 'users/password_reset.html'),
     name ='password_reset'),
 
     path('e/antemiaa/password-reset/done',
-    auth_views.PasswordResetViewDone.LogoutView.as_view(template_name= 'users/password_reset_done.html'),
+    auth_views.PasswordResetViewDone.as_view(template_name= 'users/password_reset_done.html'),
     name ='password_reset_done'),
 
     path('e/antemiaa/', include('simon.urls'))
