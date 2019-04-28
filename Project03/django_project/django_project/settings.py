832fb755117fb20173885147e88d5269e3141264
400209661 <antemiaa@mcmaster.ca>
2019-04-27 20:22:49 -0400

    imported io.path to settings.py

--- Project03/django_project/django_project/settings.py
+++ Project03/django_project/django_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import os.path
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -118,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    "Project03\django_project\simon\static\styles.css"
-]
+STATICFILES_DIRS = ( os.path.join('static'), )
