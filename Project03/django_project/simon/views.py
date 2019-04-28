f09d3a5e02f2cd22d86ca9fad4a0c3c16c556d5d
400209661 <antemiaa@mcmaster.ca>
2019-04-27 23:23:17 -0400

    Added have to be logged in to view ./profile and ./about

--- Project03/django_project/simon/views.py
+++ Project03/django_project/simon/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import Score
-
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     leaderBoard = {
         'scores': Score.objects.all()
     }
     return render(request, 'simon/home.html', leaderBoard)
-
+    
+@login_required
 def about(request):
     return render(request, 'simon/about.html', {'title': 'About'})
